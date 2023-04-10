d19bd02db46c0b3ab14c3aad412a6c4ec8ce263a
Roy Mbaya <roymbaya08@gmail.com>
2023-04-10 13:10:12 +0300

    done with challenge

--- script.js
+++ script.js
@@ -1,129 +1,128 @@
-let grade=document.getElementById('grade')
-let speed=document.getElementById('speed')
-let salary=document.getElementById('salary')
+let grade = document.getElementById("grade");
+let speed = document.getElementById("speed");
+let salary = document.getElementById("salary");
+
+grade.addEventListener("click", (e) => {
+  e.preventDefault();
+  let inputGrade = parseInt(document.getElementById("input-grade").value);
+  console.log(inputGrade);
+  let finalOutput = "marks";
 
 
-grade.addEventListener('click', (e)=>{
-e.preventDefault()
-let inputGrade=parseInt(document.getElementById('input-grade').value)
-console.log(inputGrade)
-let finalOutput
-// write Code For the Code challenge
+  if (inputGrade > 79) {
+    finalOutput = "A";
+  } else if (inputGrade >= 60) {
+    finalOutput = "B";
+  } else if (inputGrade >= 50) {
+    finalOutput = "C";
+  } else if (inputGrade >= 40) {
+    finalOutput = "D";
+  } else if (inputGrade <= 39) {
+    finalOutput = "E";
+  } else {
+    finalOutput = "Invalid Input";
+  }
 
-const prompt=require("prompt-sync")();
-let mark=prompt("please enter your marks");
-let marks=mark;
-function grade(){ 
+  console.log(finalOutput);
 
-  if (marks <=39)
-   { 
-    console.log(" Your grade is E");
-   }
-     else if (marks >=40 || marks <=49)
-{
-console.log("Your grade is D");
-}
-else if (marks >=50 || marks <=59)
-{
-console.log(" Your grade is C");
-}
-else if (marks >=60 || marks <=79)
-{
-console.log(" Your grade is B");
-}
-else if (marks >=80 || marks <=100)
-{
-console.log(" Your grade is A");
-}
-}
-
-finalOutput='assign your final output to this variable'
-document.getElementById('marks-id').append(finalOutput)
-
-})
-speed.addEventListener('click', (e)=>{
-    e.preventDefault()
-    let inputSpeed=parseInt(document.getElementById('input-speed').value)
-    console.log(inputSpeed)
-    let finalOutput
-// write Code For the Code challenge
+  
+  document.getElementById("marks-id").append(finalOutput);
+});
+speed.addEventListener("click", (e) => {
+  e.preventDefault();
+  let inputSpeed = parseInt(document.getElementById("input-speed").value);
+  console.log(inputSpeed);
+  let finalOutput = "inputSpeed";
 
 
-const prompt=require("prompt-sync")();
-let speed=prompt("Enter speed of the car");
-function points(){
-    if(speed < 70){
-        console.log("Ok")
+  let demeritPoints = 0;
+
+  if (inputSpeed < 70) {
+    finalOutput = "Ok";
+  } else {
+    demeritPoints = Math.floor((inputSpeed - 70) / 5);
+    finalOutput = `Points: ${demeritPoints}`;
+
+    if (demeritPoints >= 12) {
+      finalOutput = "License suspended";
     }
-    else if(speed > 70){
-        const penaltypoints=((speed-70)/5)
-        console.log(Math.ceil(penaltypoints));
-        if(penaltypoints >=12){
-            console.log('License suspended')
-        }
-    }
+  }
+
+  console.log(finalOutput);
+
+  
+  document.getElementById("speed-id").append(finalOutput);
+});
+salary.addEventListener("click", (e) => {
+    e.preventDefault();
+    let inputSalary = parseInt(document.getElementById("input-salary").value);
+    console.log(inputSalary);
+    let finalOutput;
+
+
+const basicSalary = parseInt(prompt("Enter your basic salary: "));
+const benefits = parseInt(prompt("Enter your benefits: "));
+
+let payee;
+if (basicSalary + benefits <= 24000) {
+  payee = 0;
+} else if (basicSalary + benefits <= 40667) {
+  payee = (basicSalary + benefits - 24000) * 0.1;
+} else if (basicSalary + benefits <= 57733) {
+  payee = 1667 + (basicSalary + benefits - 40667) * 0.15;
+} else if (basicSalary + benefits <= 73333) {
+  payee = 4250 + (basicSalary + benefits - 57733) * 0.2;
+} else if (basicSalary + benefits <= 89667) {
+  payee = 8250 + (basicSalary + benefits - 73333) * 0.25;
+} else {
+  payee = 14050 + (basicSalary + benefits - 89667) * 0.3;
 }
 
-finalOutput='assign your final output to this variable'
-    document.getElementById('speed-id').append(finalOutput)
 
-})
-salary.addEventListener('click', (e)=>{
-        e.preventDefault()
-        let inputSalary=parseInt(document.getElementById('input-salary').value)
-        console.log(inputSalary)
-let finalOutput
-// write Code For the Code challenge
-
-
-const prompt=require("prompt-sync")();
-let pay=prompt("please enter your grosSalary")
-let grosSalary=pay;
-function SalaryPayable(){
-let paye;
-let nssf =400;
 let nhif;
-let deductions;
+if (basicSalary + benefits < 6000) {
+  nhif = 150;
+} else if (basicSalary + benefits < 8000) {
+  nhif = 300;
+} else if (basicSalary + benefits < 12000) {
+  nhif = 400;
+} else if (basicSalary + benefits < 15000) {
+  nhif = 500;
+} else if (basicSalary + benefits < 20000) {
+  nhif = 600;
+} else if (basicSalary + benefits < 25000) {
+  nhif = 750;
+} else if (basicSalary + benefits < 30000) {
+  nhif = 850;
+} else if (basicSalary + benefits < 35000) {
+  nhif = 900;
+} else if (basicSalary + benefits < 40000) {
+  nhif = 950;
 
-if (grosSalary<24000){
-    paye=(grosSalary*0.1)
-}
-else if(grosSalary< 32332 && grosSalary >24000){
-    paye= (grosSalary*0.25)
-}
-else if (grosSalary>32333){
-    paye=(grosSalary*0.3)
-}
-else if (grosSalary< 6000){
-        nhif=150;
-    }
-else if(grosSalary>=6000 && grosSalary<8000){
-    nhif=300 ;   
-}
-else if(grosSalary>=8000 && grosSalary<12000){
-    nhif= 400;
-}
-else if(grosSalary>=12000 && grosSalary<15000){
-    nhif=500;
-}
-else if( grosSalary>=15000 && grosSalary<20000){
-    nhif=600;
-}
-else if(grosSalary>=20000 && grosSalary<25000){
-        nhif=700;
-    }
-else if(grosSalary>=25000 && grosSalary <30000){
-    nhif=850;
-}
-deductions= (paye+nhif+nssf)
 
-const netSalary=(grosSalary-deductions)
-        console.log(netSalary);
+} else {
+nhif = 1000;
 }
 
-SalaryPayable()
 
-finalOutput ='assign your final output to this variable'
-document.getElementById('salary-id').append(finalOutput)
-        
-})
+const nssf = basicSalary * 0.06;
+
+
+const grossPay = basicSalary + benefits;
+const totalDeductions = payee + nhif + nssf;
+const netPay = grossPay - totalDeductions;
+
+
+console.log(`Basic Salary: ${basicSalary}`);
+console.log(`Benefits: ${benefits}`);
+console.log(`Gross Pay: ${grossPay}`);
+console.log(`Payee: ${payee}`);
+console.log(`NHIF: ${nhif}`);
+console.log(`NSSF: ${nssf}`);
+console.log(`Total Deductions: ${totalDeductions}`);
+console.log(`Net Pay: ${netPay}`);
+finalOutput = `${netPay}`;
+console.log("finalOutput");
+  
+  document.getElementById("salary-id").append(finalOutput);
+});
